a9900d81cc5410f70a8cf8f8de289ab1363aab3a
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-24 09:51:45 +0700

    feat: create register form component and register page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
-import { Heading, Center } from '@chakra-ui/react';
+import { Heading, VStack, Link } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 export default function Home() {
   return (
-    <Center w='100%' h='100vh'>
-      <Heading>Homepage</Heading>
-    </Center>
+    <VStack w='100%' h='100vh' justifyContent='center' alignItems='center'>
+      <Heading mb='6'>Homepage</Heading>
+      <Link
+        as={NextLink}
+        href='/register'
+        color='orange.500'
+        textDecoration='underline'
+        _hover={{ color: 'orange.200' }}
+      >
+        Register
+      </Link>
+    </VStack>
   );
 }
