cc285343cef34a435d7bf403d69cc50e26500d4c
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-26 15:42:20 +0700

    feat: create card and card list using static data

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,14 @@
 import { Stack, useBreakpointValue } from '@chakra-ui/react';
 import Navbar from '@/components/Navbar';
 import NavbarMobile from '@/components/NavbarMobile';
+import CardList from '@/components/CardList';
 
 export default function Home() {
   const isSmallScreen = useBreakpointValue({ base: true, md: false });
-  return <Stack>{isSmallScreen ? <NavbarMobile /> : <Navbar />}</Stack>;
+  return (
+    <Stack>
+      {isSmallScreen ? <NavbarMobile /> : <Navbar />}
+      <CardList />
+    </Stack>
+  );
 }
