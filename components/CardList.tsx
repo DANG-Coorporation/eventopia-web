e8789c8b37b50802678f9769933194c60e578d92
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-27 02:15:25 +0700

    style: edit card properties

--- components/CardList.tsx
+++ components/CardList.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { SimpleGrid } from '@chakra-ui/react';
+import {
+  SimpleGrid,
+  Stack,
+  Heading,
+  useBreakpointValue,
+} from '@chakra-ui/react';
 import Card from './Card';
 
 export default function CardList() {
   return (
-    <SimpleGrid
-      columns={{ base: 1, sm: 2, md: 3, lg: 4 }}
-      spacing='6'
-      px={{ base: 6, lg: 24 }}
-    >
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-    </SimpleGrid>
+    <Stack w='100%' px={{ base: 6, lg: 24 }}>
+      <Heading as='h2' size='lg' my='4'>
+        Events
+      </Heading>
+      <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing='6'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </SimpleGrid>
+    </Stack>
   );
 }
